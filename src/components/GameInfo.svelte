614bce9bfd85e469c163e26ad072856da160316e
CleitonLuizOliveira <cleitoneldorn@gmail.com>
2021-02-22 16:50:10 -0300

    interface do jogo pronta

--- src/components/GameInfo.svelte
+++ src/components/GameInfo.svelte
@@ -1,4 +1,6 @@
 <script>
+    import pokeball from '../images/pokeball.png';
+
     export let lives;
     export let score;
 </script>
@@ -13,13 +15,38 @@
     .score {
         text-align: right;
     }
+
+    legend {
+        padding: 0;
+        margin: 0;
+    }
+
+    h2 {
+        font-weight: bolder;
+        font-size: 32px;
+        margin: 0;
+        padding: 0;
+    }
+
+    .life-counter {
+        display: block;
+        height: 32px;
+        width: 96px;
+        background-position: top right;
+        background-repeat: repeat;
+    }
+
+
 </style>
 
 <div class="container">
     <div class="lives">
-        <legend>lives: {lives}</legend>
+        <legend>lives: </legend>
+        <div style="background-image: url({pokeball}); width: {32 * lives}px" class="life-counter"></div>
     </div>
     <div class="score">
-        <legend>score: {score}</legend>
+        <legend>score:</legend>
+        <h2>{score}</h2>
     </div>
+    
 </div>
